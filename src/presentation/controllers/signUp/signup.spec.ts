3ba573b33863878355a06708eb91882b37b22378
Idyl Icaro dos Santos <idylicaro.se@hotmail.com>
2020-07-09 18:30:37 -0300

    test: ensure return statuscode 400 if email is not passed

--- src/presentation/controllers/signUp/signup.spec.ts
+++ src/presentation/controllers/signUp/signup.spec.ts
@@ -6,11 +6,24 @@ const makeSut = (): SignUpController => {
 }
 
 describe('SignUp Controller', () => {
-  test('Should return 400 if named is not passed', async () => {
+  test('Should return 400 if name is not passed', async () => {
     const sut = makeSut()
     const httpRequest = {
       body: {
         email: 'any_email@mail.com',
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
+      }
+    }
+    const httpResponse = await sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+  })
+
+  test('Should return 400 if email is not passed', async () => {
+    const sut = makeSut()
+    const httpRequest = {
+      body: {
+        name: 'any_name',
         password: 'any_password',
         passwordConfirmation: 'any_password'
       }
