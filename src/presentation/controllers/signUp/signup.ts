c94733e04518ee94faf042fc618a05f074435c15
Idyl Icaro dos Santos <idylicaro.se@hotmail.com>
2020-07-10 20:36:39 -0300

    feat: ensure return 400 if email is not valid

--- src/presentation/controllers/signUp/signup.ts
+++ src/presentation/controllers/signUp/signup.ts
@@ -1,8 +1,14 @@
 import { Controller } from '../../protocols/controller'
 import { HttpRequest, HttpResponse } from '../../protocols/http'
 import { ok } from '../../helpers/http-helper'
+import { Validator } from '../../protocols/validator'
 
 export class SignUpController implements Controller {
+  private readonly validator: Validator
+  constructor (validator: Validator) {
+    this.validator = validator
+  }
+
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
     for (const field of requiredFields) {
@@ -13,8 +19,14 @@ export class SignUpController implements Controller {
         }))
       }
     }
-    const { password, passwordConfirmation } = httpRequest.body
+    const { email, password, passwordConfirmation } = httpRequest.body
     if (password !== passwordConfirmation) {
+      return await new Promise(resolve => resolve({
+        statusCode: 400,
+        body: httpRequest.body
+      }))
+    }
+    if (!this.validator.isValid(email)) {
       return await new Promise(resolve => resolve({
         statusCode: 400,
         body: httpRequest.body
---
+++ src/presentation/protocols/validator.ts
@@ -0,0 +1,3 @@
+export interface Validator {
+  isValid: (email: string) => boolean
+}
