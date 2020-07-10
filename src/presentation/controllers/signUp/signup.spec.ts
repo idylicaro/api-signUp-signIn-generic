50e567de41a5bf57137279f63ab22c99a363a20d
Idyl Icaro dos Santos <idylicaro.se@hotmail.com>
2020-07-10 20:37:13 -0300

    test: ensure return 400 if email is not valid

--- src/presentation/controllers/signUp/signup.spec.ts
+++ src/presentation/controllers/signUp/signup.spec.ts
@@ -1,13 +1,35 @@
 
 import { SignUpController } from './signup'
+import { Validator } from '../../protocols/validator'
 
-const makeSut = (): SignUpController => {
-  return new SignUpController()
+const makeValidator = (): Validator => {
+  // factory
+  class ValidatorStub implements Validator {
+    // mock  type stub
+    isValid (email: string): boolean {
+      return true
+    }
+  }
+  return new ValidatorStub()
+}
+
+interface SutTypes {
+  sut: SignUpController
+  validatorStub: Validator
+}
+
+const makeSut = (): SutTypes => {
+  const validatorStub = makeValidator()
+  const sut = new SignUpController(validatorStub)
+  return {
+    sut,
+    validatorStub
+  }
 }
 
 describe('SignUp Controller', () => {
   test('Should return 400 if name is not passed', async () => {
-    const sut = makeSut()
+    const { sut } = makeSut()
     const httpRequest = {
       body: {
         email: 'any_email@mail.com',
@@ -20,7 +42,7 @@ describe('SignUp Controller', () => {
   })
 
   test('Should return 400 if email is not passed', async () => {
-    const sut = makeSut()
+    const { sut } = makeSut()
     const httpRequest = {
       body: {
         name: 'any_name',
@@ -33,7 +55,7 @@ describe('SignUp Controller', () => {
   })
 
   test('Should return 400 if password is not passed', async () => {
-    const sut = makeSut()
+    const { sut } = makeSut()
     const httpRequest = {
       body: {
         name: 'any_name',
@@ -46,7 +68,7 @@ describe('SignUp Controller', () => {
   })
 
   test('Should return 400 if password confimation is not passed', async () => {
-    const sut = makeSut()
+    const { sut } = makeSut()
     const httpRequest = {
       body: {
         name: 'any_name',
@@ -59,13 +81,28 @@ describe('SignUp Controller', () => {
   })
 
   test('Should return 400 if password confimation fails', async () => {
-    const sut = makeSut()
+    const { sut } = makeSut()
     const httpRequest = {
       body: {
         name: 'any_name',
         email: 'any_email@mail.com',
         password: 'any_password',
         passwordConfirmation: 'different_password'
+      }
+    }
+    const httpResponse = await sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+  })
+
+  test('Should return 400 if email is not valid', async () => {
+    const { sut, validatorStub } = makeSut()
+    jest.spyOn(validatorStub, 'isValid').mockReturnValueOnce(false)
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        email: 'invalid_email@mail.com',
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
       }
     }
     const httpResponse = await sut.handle(httpRequest)
