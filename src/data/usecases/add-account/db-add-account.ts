28c826c7f3d2d043882babfa674f19b6c9842102
Idyl Icaro dos Santos <idylicaro.se@hotmail.com>
2020-07-19 16:13:28 -0300

    feat: ensure call encrypter with correct password

---
+++ src/data/usecases/add-account/db-add-account.ts
@@ -0,0 +1,12 @@
+import { AddAccount, AccountModel, AddAccountModel, Encrypter } from './db-add-account-protocols'
+export class DbAddAccount implements AddAccount {
+  private readonly encrypter: Encrypter
+  constructor (encrypter: Encrypter) {
+    this.encrypter = encrypter
+  }
+
+  async add (account: AddAccountModel): Promise<AccountModel> {
+    await this.encrypter.encrypt(account.password)
+    return await Promise.resolve(null)
+  }
+}
