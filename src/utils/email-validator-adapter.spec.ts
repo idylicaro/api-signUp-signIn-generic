d74a3f9f6f80a1c656a165d8de8525a4f54d19d6
Idyl Icaro dos Santos <idylicaro.se@hotmail.com>
2020-07-19 16:11:45 -0300

    fix: fix test name duplicate

--- src/utils/email-validator-adapter.spec.ts
+++ src/utils/email-validator-adapter.spec.ts
@@ -11,19 +11,19 @@ const makeSut = (): EmailValidatorAdapter => {
 }
 
 describe('EmailValidator Adapter', () => {
-  test('Should return false if validator returns false', () => {
+  test('Should return false if emailvalidator returns false', () => {
     const sut = makeSut()
     jest.spyOn(validator, 'isEmail').mockReturnValueOnce(false)
     const isValid = sut.isValid('invalid_email@mail.com')
     expect(isValid).toBe(false)
   })
 
-  test('Should return true if validator returns true', () => {
+  test('Should return true if emailvalidator returns true', () => {
     const sut = makeSut()
     const isValid = sut.isValid('valid_email@mail.com')
     expect(isValid).toBe(true)
   })
-  test('Should return true if validator returns true', () => {
+  test('Should call emailvalidator correct email', () => {
     const sut = makeSut()
     const isEmailSpy = jest.spyOn(validator, 'isEmail')
     sut.isValid('valid_email@mail.com')
