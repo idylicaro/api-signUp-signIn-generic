98d33583715dcef4be22aec01fbfc771a3377f2b
Idyl Icaro dos Santos <idylicaro.se@hotmail.com>
2020-07-20 16:20:28 -0300

    test: ensure Bcrypt call with correctly values

---
+++ src/infra/criptography/bcrypt-adapter.spec.ts
@@ -0,0 +1,22 @@
+import bcrypt from 'bcrypt'
+import { BcryptAdapter } from './bcrypt-adapter'
+
+jest.mock('bcrypt', () => ({
+  async hash (): Promise<string> {
+    return await new Promise(resolve => resolve('hash'))
+  }
+}))
+
+const salt = 12
+const makeSut = (): BcryptAdapter => {
+  return new BcryptAdapter(salt)
+}
+
+describe('Bcrypt Adapter', () => {
+  test('Should call bcrypt with correct values', async () => {
+    const sut = makeSut()
+    const hashSpy = jest.spyOn(bcrypt, 'hash')
+    await sut.encrypt('any_value')
+    expect(hashSpy).toHaveBeenCalledWith('any_value', salt)
+  })
+})
