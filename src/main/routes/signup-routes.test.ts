89e7d684f0c786c10317d094f0a25cfcc5dac403
Idyl Icaro dos Santos <idylicaro.se@hotmail.com>
2020-07-30 13:55:24 -0300

    feat: ensure dynamic import of routes

---
+++ src/main/routes/signup-routes.test.ts
@@ -0,0 +1,10 @@
+import request from 'supertest'
+import app from '../config/app'
+
+describe('SignUp Routes', () => {
+  test('Should return an account on success', async () => {
+    await request(app)
+      .post('/api/signup')
+      .expect(200)
+  })
+})
