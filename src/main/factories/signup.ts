39c097abf49b0a1e1d210871dd07f46b09b98be1
Idyl Icaro dos Santos <idylicaro.se@hotmail.com>
2020-07-31 18:50:20 -0300

    feat: adapter route using signupFactory

---
+++ src/main/factories/signup.ts
@@ -0,0 +1,18 @@
+import { SignUpController } from '../../presentation/controllers/signup/signup'
+import { EmailValidatorAdapter } from '../../utils/email-validator-adapter'
+import { PhoneValidatorAdapter } from '../../utils/phone-validator-adapter'
+import { DbAddAccount } from '../../data/usecases/add-account/db-add-account'
+import { BcryptAdapter } from '../../infra/criptography/bcrypt-adapter'
+import { AccountPostgreRepository } from '../../infra/db-postgresql/account-repository/account'
+import { Controller } from '../../presentation/protocols'
+
+export const makeSignUpController = (): Controller => {
+  const salt = 12
+  const emailValidatorAdapter = new EmailValidatorAdapter()
+  const phoneValidatorAdapter = new PhoneValidatorAdapter()
+  const bcryptAdapter = new BcryptAdapter(salt)
+  const accountMongoRepository = new AccountPostgreRepository()
+  const dbAddAccount = new DbAddAccount(bcryptAdapter, accountMongoRepository)
+  const signUpController = new SignUpController(emailValidatorAdapter, phoneValidatorAdapter, dbAddAccount)
+  return signUpController
+}
