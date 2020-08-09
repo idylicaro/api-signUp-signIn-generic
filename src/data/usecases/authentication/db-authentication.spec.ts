6af9e65298b588845c6d3d3c73bf9b865e013a36
Idyl Icaro dos Santos <idylicaro.se@hotmail.com>
2020-08-09 20:50:47 -0300

    test: ensure DbAuthentication return a token on success

--- src/data/usecases/authentication/db-authentication.spec.ts
+++ src/data/usecases/authentication/db-authentication.spec.ts
@@ -130,4 +130,10 @@ describe('DbAuthenticarion Usecase', () => {
     const promise = sut.auth(makeFakeAuthentication())
     await expect(promise).rejects.toThrow()
   })
+
+  test('Should return token if succeeds', async () => {
+    const { sut } = makeSut()
+    const accessToken = await sut.auth(makeFakeAuthentication())
+    expect(accessToken).toBe('any_token')
+  })
 })
