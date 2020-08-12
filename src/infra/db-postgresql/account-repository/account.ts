fb483d81695fa1f68a10fdec8b6a2f9a79b20563
Idyl Icaro dos Santos <idylicaro.se@hotmail.com>
2020-08-12 19:01:43 -0300

    feat: loadByEmail in accountRepository

--- src/infra/db-postgresql/account-repository/account.ts
+++ src/infra/db-postgresql/account-repository/account.ts
@@ -1,9 +1,10 @@
 import { AddAccountRepository } from '../../../data/protocols/db/add-account-respository'
 import { AddAccountModel } from '../../../domain/usecases/add-account'
 import { AccountModel } from '../../../domain/models/account'
+import { LoadAccountByEmailRepository } from '../../../data/protocols/db/load-account-by-email-repository'
 import knex from '../knex'
 
-export class AccountPostgreRepository implements AddAccountRepository {
+export class AccountPostgreRepository implements AddAccountRepository, LoadAccountByEmailRepository {
   async add (accountData: AddAccountModel): Promise<AccountModel> {
     // let id: string
     const [user] = await knex('accounts').insert({
@@ -13,6 +14,11 @@ export class AccountPostgreRepository implements AddAccountRepository {
       password: accountData.password
     }).returning('*')
 
+    return user
+  }
+
+  async loadByEmail (email: string): Promise<AccountModel> {
+    const [user] = await knex('accounts').where('email', email)
     return user
   }
 }
