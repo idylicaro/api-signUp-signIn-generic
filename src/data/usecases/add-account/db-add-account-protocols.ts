68dba35c1e14c4bee9f2a30298c88c77d51764ad
Idyl Icaro dos Santos <idylicaro.se@hotmail.com>
2020-08-13 15:40:33 -0300

    refactor: improve file names

--- src/data/usecases/add-account/db-add-account-protocols.ts
+++ src/data/usecases/add-account/db-add-account-protocols.ts
@@ -1,4 +1,4 @@
 export * from '../../../domain/usecases/add-account'
 export * from '../../../domain/models/account'
 export * from '../../protocols/criptography/hasher'
-export * from '../../protocols/db/add-account-respository'
+export * from '../../protocols/db/account/add-account-respository'
--- src/main/factories/signup/signup-factory.ts
+++ src/main/factories/signup/signup-factory.ts
@@ -1,9 +1,9 @@
-import { SignUpController } from '../../../presentation/controllers/signup/signup'
+import { SignUpController } from '../../../presentation/controllers/signUp/signup-controller'
 import { DbAddAccount } from '../../../data/usecases/add-account/db-add-account'
 import { BcryptAdapter } from '../../../infra/criptography/bcrypt-adapter/bcrypt-adapter'
 import { AccountPostgreRepository } from '../../../infra/db-postgresql/account/account-postgre-repository'
 import { Controller } from '../../../presentation/protocols'
-import { makeSignUpValidation } from './signup-validation'
+import { makeSignUpValidation } from './signup-validation-factory'
 
 export const makeSignUpController = (): Controller => {
   const salt = 12
