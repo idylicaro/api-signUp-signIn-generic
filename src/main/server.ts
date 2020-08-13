bdb0a1dafde534ed017661f9d39b8ff809435558
Idyl Icaro dos Santos <idylicaro.se@hotmail.com>
2020-08-13 16:45:23 -0300

    feat: create login route

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,3 +1,3 @@
 import app from './config/app'
 
-app.listen(5050, () => console.log('Server running at http:localhost:5050'))
+app.listen(5050, () => console.log('Server running at http://localhost:5050/api'))
