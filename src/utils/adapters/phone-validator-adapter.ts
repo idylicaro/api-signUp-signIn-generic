b27d28cb242e7687300d1665d9490e6efa6a2a2a
Idyl Icaro dos Santos <idylicaro.se@hotmail.com>
2020-08-13 15:49:24 -0300

    refactor: add folder adapters in utils

--- src/utils/adapters/phone-validator-adapter.ts
+++ src/utils/adapters/phone-validator-adapter.ts
@@ -1,4 +1,4 @@
-import { PhoneValidator } from '../presentation/protocols/phone-validator'
+import { PhoneValidator } from '../../presentation/protocols/phone-validator'
 import PhoneBrValidator from 'br-phone-validator-lib'
 export class PhoneValidatorAdapter implements PhoneValidator {
   isValid (phone: string): boolean {
