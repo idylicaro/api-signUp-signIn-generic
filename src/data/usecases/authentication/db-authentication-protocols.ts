d7de91d9ff7d0062d95f7a7ec1a68989e6a5a1dd
Idyl Icaro dos Santos <idylicaro.se@hotmail.com>
2020-08-16 20:20:21 -0300

    fix: rename imports of account model

--- src/data/usecases/authentication/db-authentication-protocols.ts
+++ src/data/usecases/authentication/db-authentication-protocols.ts
@@ -1,4 +1,4 @@
-export * from '../../../domain/models/Account'
+export * from '../../../domain/models/account'
 export * from '../../../domain/usecases/authentication'
 export * from '../../protocols/db/account/load-account-by-email-repository'
 export * from '../../protocols/db/account/update-access-token-repository'
