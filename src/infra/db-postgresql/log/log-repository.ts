9fe73610f9915ab6a44573a1f256070fc048a54a
Idyl Icaro dos Santos <idylicaro.se@hotmail.com>
2020-08-17 14:23:28 -0300

    feat: logger decorator

--- src/infra/db-postgresql/log/log-repository.ts
+++ src/infra/db-postgresql/log/log-repository.ts
@@ -1,7 +1,7 @@
 import { LogErrorRepository } from '../../../data/protocols/db/log/log-error-respository'
 import knex from '../knex'
 
-export class LogMongoRepository implements LogErrorRepository {
+export class LogPostgreRepository implements LogErrorRepository {
   async logError (stack: string): Promise<void> {
     await knex('errors').insert({
       stack,
