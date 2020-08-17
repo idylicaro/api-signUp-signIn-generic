ae0d2675529771b32d0d8d414bc07c02ec496eb1
Idyl Icaro dos Santos <idylicaro.se@hotmail.com>
2020-08-17 13:57:27 -0300

    feat: ensure LogController Decorator calls controller handle

---
+++ src/infra/db-postgresql/log/log-repository.ts
@@ -0,0 +1,11 @@
+import { LogErrorRepository } from '../../../data/protocols/db/log/log-error-respository'
+import knex from '../knex'
+
+export class LogMongoRepository implements LogErrorRepository {
+  async logError (stack: string): Promise<void> {
+    await knex('errors').insert({
+      stack,
+      date: new Date()
+    })
+  }
+}
---
+++ src/infra/migrations/01_create-log.ts
@@ -0,0 +1,14 @@
+import Knex from 'knex'
+
+export async function up (knex: Knex): Promise<void> {
+  // Criar a tabela
+  return await knex.schema.createTable('errors', table => {
+    table.increments('id').primary()
+    table.string('stack')
+    table.string('date')
+  })
+}
+export async function down (knex: Knex): Promise<void> {
+  // UNDO
+  return await knex.schema.dropTable('errors')
+}
