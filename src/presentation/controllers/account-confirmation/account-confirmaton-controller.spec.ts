aaa14ad318888771f6fe7031e5220215ee328855
Idyl Icaro dos Santos <idylicaro.se@hotmail.com>
2020-08-24 21:05:48 -0300

    test: ensure return 400 with MissingParamError if the id are not passed

---
+++ src/presentation/controllers/account-confirmation/account-confirmaton-controller.spec.ts
@@ -0,0 +1,26 @@
+import { AccountConfirmationController } from './account-confirmaton-controller'
+import { badRequest } from '../../helpers/http/http-helper'
+import { MissingParamError } from '../../errors/missing-param-error'
+interface SutTypes {
+  sut: AccountConfirmationController
+}
+
+const makeSut = (): SutTypes => {
+  const sut = new AccountConfirmationController()
+  return {
+    sut
+  }
+}
+
+describe('Account Confirmation Controller', () => {
+  test('Should return 400 with MissingParamError if the id are not passed', async () => {
+    const { sut } = makeSut()
+    const httpRequest = {
+      query: {
+        token: 'any_token'
+      }
+    }
+    const httpResponse = await sut.handle(httpRequest)
+    expect(httpResponse).toEqual(badRequest(new MissingParamError('id')))
+  })
+})
