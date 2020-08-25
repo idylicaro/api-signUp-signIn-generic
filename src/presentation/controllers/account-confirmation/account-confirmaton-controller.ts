11a3fa924406a52943722332d44653d2cf556265
Idyl Icaro dos Santos <idylicaro.se@hotmail.com>
2020-08-25 15:34:10 -0300

    feat: ensure AccountConfirmationController calls AccountConfirmation with correct values

--- src/presentation/controllers/account-confirmation/account-confirmaton-controller.ts
+++ src/presentation/controllers/account-confirmation/account-confirmaton-controller.ts
@@ -1,15 +1,19 @@
 import { Controller, HttpRequest, HttpResponse } from '../../protocols'
 import { badRequest } from '../../helpers/http/http-helper'
 import { MissingParamError } from '../../errors'
+import { AccountConfirmation } from '../../../domain/usecases/account-confirmation'
 
 export class AccountConfirmationController implements Controller {
+  constructor (private readonly accountConfirmation: AccountConfirmation) {}
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    if (!httpRequest.query.id) {
+    const { id, token } = httpRequest.query
+    if (!id) {
       return badRequest(new MissingParamError('id'))
     }
-    if (!httpRequest.query.token) {
+    if (!token) {
       return badRequest(new MissingParamError('token'))
     }
+    await this.accountConfirmation.confirm(id, token)
     return await new Promise(resolve => resolve(null))
   }
 }
