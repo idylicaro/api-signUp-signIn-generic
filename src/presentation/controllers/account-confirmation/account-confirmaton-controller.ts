6254fb61c4e108ce037dda386c0c3bcea2b2bdfc
Idyl Icaro dos Santos <idylicaro.se@hotmail.com>
2020-08-27 15:55:53 -0300

    feat: ensure return serverError if AccountConfirmation throws

--- src/presentation/controllers/account-confirmation/account-confirmaton-controller.ts
+++ src/presentation/controllers/account-confirmation/account-confirmaton-controller.ts
@@ -1,22 +1,26 @@
 import { Controller, HttpRequest, HttpResponse } from '../../protocols'
-import { badRequest, ok } from '../../helpers/http/http-helper'
+import { badRequest, ok, serverError } from '../../helpers/http/http-helper'
 import { MissingParamError, InvalidError } from '../../errors'
 import { AccountVerify } from '../../../domain/usecases/verify-account'
 
 export class AccountConfirmationController implements Controller {
   constructor (private readonly accountConfirmation: AccountVerify) {}
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    const { id, token } = httpRequest.query
-    if (!id) {
-      return badRequest(new MissingParamError('id'))
+    try {
+      const { id, token } = httpRequest.query
+      if (!id) {
+        return badRequest(new MissingParamError('id'))
+      }
+      if (!token) {
+        return badRequest(new MissingParamError('token'))
+      }
+      const isConfirmated = await this.accountConfirmation.confirm(id, token)
+      if (!isConfirmated) {
+        return badRequest(new InvalidError())
+      }
+      return ok({ Message: 'Confirmation Accepted' })
+    } catch (error) {
+      return serverError(error)
     }
-    if (!token) {
-      return badRequest(new MissingParamError('token'))
-    }
-    const isConfirmated = await this.accountConfirmation.confirm(id, token)
-    if (!isConfirmated) {
-      return badRequest(new InvalidError())
-    }
-    return ok({ Message: 'Confirmation Accepted' })
   }
 }
