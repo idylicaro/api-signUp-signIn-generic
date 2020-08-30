c138604855494d16ebcc90f2f5994cb1263077ee
Idyl Icaro dos Santos <idylicaro.se@hotmail.com>
2020-08-30 19:33:05 -0300

    feat: ensure DbAccountConfirmation return false if AccountConfirmation has expired

--- src/data/usecases/account-confirmation/db-account-confirmation.ts
+++ src/data/usecases/account-confirmation/db-account-confirmation.ts
@@ -17,6 +17,16 @@ export class DbAccountConfirmation implements AccountVerify {
     if (!accountConfirmation) {
       return false
     }
+
+    const strDateExpire = accountConfirmation.date_expire.split('-')
+    const dateExpire = strDateExpire.map(item => parseInt(item))
+    const currentDate = new Date()
+
+    if (!(dateExpire[0] >= currentDate.getDate() &&
+        dateExpire[1] >= (currentDate.getMonth() + 1) &&
+        dateExpire[3] >= currentDate.getFullYear())) {
+      return false
+    }
     return true
   }
 }
