115a9221299f5d5ce356b657c01a0b703e96ce5a
Idyl Icaro dos Santos <idylicaro.se@hotmail.com>
2020-09-06 14:00:02 -0300

    feat: dbaccount methods

---
+++ src/data/protocols/db/account/index.ts
@@ -0,0 +1,5 @@
+export * from './add-account-respository'
+export * from './load-account-by-email-repository'
+export * from './load-account-by-id-repository'
+export * from './update-access-token-repository'
+export * from './confirm-account-repository'
