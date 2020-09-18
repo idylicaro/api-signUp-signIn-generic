ffe2b19af8d5fc23ae34eeef7ff85b99265b175a
Idyl Icaro dos Santos <idylicaro.se@hotmail.com>
2020-09-18 18:01:58 -0300

    feat: ensure account confirmation controller calls validation with correct value

--- src/presentation/controllers/account-confirmation/account-confirmaton-controller.ts
+++ src/presentation/controllers/account-confirmation/account-confirmaton-controller.ts
@@ -2,11 +2,16 @@ import { Controller, HttpRequest, HttpResponse } from '../../protocols'
 import { badRequest, ok, serverError } from '../../helpers/http/http-helper'
 import { MissingParamError, InvalidError } from '../../errors'
 import { AccountVerify } from '../../../domain/usecases/verify-account'
+import { Validation } from '../../protocols/validation'
 
 export class AccountConfirmationController implements Controller {
-  constructor (private readonly accountConfirmation: AccountVerify) {}
+  constructor (
+    private readonly accountConfirmation: AccountVerify,
+    private readonly validation: Validation) {}
+
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
+      this.validation.validate(httpRequest.query)
       const { id } = httpRequest.query
       if (!id) {
         return badRequest(new MissingParamError('id'))
