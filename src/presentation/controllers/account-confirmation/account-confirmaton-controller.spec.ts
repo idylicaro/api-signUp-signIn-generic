8ddce7178c25f3e12d111ae1c989dfe9cb38a69a
Idyl Icaro dos Santos <idylicaro.se@hotmail.com>
2020-09-18 18:08:58 -0300

    test: ensure account confirmation controller return 400 if validation returns an error

--- src/presentation/controllers/account-confirmation/account-confirmaton-controller.spec.ts
+++ src/presentation/controllers/account-confirmation/account-confirmaton-controller.spec.ts
@@ -98,4 +98,11 @@ describe('Account Confirmation Controller', () => {
     await sut.handle(httpRequest)
     expect(validateSpy).toHaveBeenCalledWith(httpRequest.query)
   })
+
+  test('Should return 400 if Validation returns an error', async () => {
+    const { sut, validationStub } = makeSut()
+    jest.spyOn(validationStub, 'validate').mockReturnValueOnce(new MissingParamError('any_field'))
+    const httpResponse = await sut.handle(makeFakeHttpRequest())
+    expect(httpResponse).toEqual(badRequest(new MissingParamError('any_field')))
+  })
 })
